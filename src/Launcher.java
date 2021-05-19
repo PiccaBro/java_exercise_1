aa90a14cf71723cde404191f69cfe043e8c5c307
Marius Mihai <marius.mihai@epita.fr>
2021-05-19 12:03:11 +0200

    Partie 4 Fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,15 +1,38 @@
 public class Launcher {
+    static java.util.Scanner input = new java.util.Scanner(System.in);
+    public static int fibo_rec(int n){
+        int f_0 = 0;
+        int f_1 = 1;
+        if(n == 0){
+            return f_0;
+        }
+        if (n==1){
+            return f_1;
+        }
+        return fibo_rec(n-1)+fibo_rec(n-2);
+    }
+    public static int f_fibo(){
+        System.out.println("Please enter a number from 0 to infinity");
+        int number = input.nextInt();
+        input.nextLine();
+        int result = fibo_rec(number);
+        System.out.println(result);
+        return 0;
+    }
     public static void main(String[] args) {
         System.out.println("Bienvenue!");
-        var input = new java.util.Scanner(System.in);
         String text;
         while (true) {
             text = input.nextLine();
-            if (text.equals("quit")) {
-                break;
-            }
-            else {
-                System.out.println("Unknown command");
+            switch (text) {
+                case "quit":
+                    return;
+                case "fibo":
+                    f_fibo();
+                    break;
+                default:
+                    System.out.println("Unknown command");
+                    break;
             }
         }
     }
