0e681cbb12a132b7abd1ebbceff4e440a578db9b
Marius Mihai <marius.mihai@epita.fr>
2021-05-19 11:12:54 +0200

    Partie 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Launcher {
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue!");
+        var input = new java.util.Scanner(System.in);
+        String text;
+        while (true) {
+            text = input.nextLine();
+            if (text.equals("quit")) {
+                break;
+            }
+            else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
 }
